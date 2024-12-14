e6ccf4ddd51f1e9d4a65ae192abdd58a75cddbe3
Oleksii Khodus <exalchel@gmail.com>
2024-12-14 23:05:43 +0200

    v1.02 (added GUI as tkinter but with error)

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import time as t
+import tkinter as tk
 
-def tomate_timer(work_minutes, break_minutes, cycles = 4):
-    for cycle in range(1, cycles + 1):
-        print(f"Cycle {cycle}/{cycles}: work time is - {work_minutes}")
-        t.sleep(work_minutes * 60)
-        print("Break time!")
-        t.sleep(break_minutes * 60)
-    print("That's all the work is done, good job!")
+root = tk.Tk()
 
-if __name__ == "__main__":
-    work_time = int(input("Enter duration of your work (in minutes): "))
-    break_time = int(input("Enter duration of your break (in minutes): "))
-    tomate_timer(work_time, break_time)
+root.title("Tomate!")
+root.geometry("490x270")
+# 1920x1080 / 4
+
+label = tk.Label(root, text="Welcome to Tomate! \n \
+Please enter duration of work and break (in minutes)")
+
+label.pack()
+
+work_entry = tk.Entry(root)
+work_entry.pack()
+
+break_entry = tk.Entry(root)
+break_entry.pack()
+
+
+def start_timer():
+    try:
+        user_work = int(work_entry.get() * 60)
+        user_break = int(break_entry.get() * 60)
+        tomate_timer(user_work, user_break, 1)
+        
+    except ValueError:
+        label.config(text="Please enter valid number!")
+
+
+def tomate_timer(work_t, break_t, loop, loops = 4):
+    if loop > loops:
+        label.config(text="That's all the work is done, good job!")
+        return
+
+    label.config(text=f"Round number {loop}: work time is - {work_t // 60} min")
+    root.after(work_t * 1000, start_break, break_t, loop, loops)
+
+    def start_break(break_t, loop, loops):
+        label.config(text="Break time!")
+        root.after(break_t * 1000,tomate_timer, break_t, break_t, loop + 1, loops)
+        
+
+button = tk.Button(root, text="Push", command=start_timer)
+button.pack()
+
+root.mainloop()
